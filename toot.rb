36ce8025390d343cbc844b6ee6d38b4e11dfbf5a
Sora0920 <sora0920@users.noreply.github.com>
2017-10-16 00:07:47 +0900

    Update toot.rb

--- toot.rb
+++ toot.rb
@@ -1,55 +1,32 @@
 require 'net/http'
 require 'uri'
 require 'json'
-require 'optparse'
 
-vis = "public"
-cw = ""
-token = ""
-host = ''
 
-OptionParser.new do |opt|
-  opt.on('--pb',            'Specify visibility as public'                      ) { vis = "public" }
-  opt.on('--ul',            'Specify visibility as unlisted'                    ) { vis = "unlisted" }
-  opt.on('--pv',            'Specify visibility as private'                     ) { vis = "private" }
-  opt.on('--di',            'Specify visibility as direct'                      ) { vis = "direct" }
-  opt.on('--cw [TEXT]',     'Use CW. Please Input CW Text for After this option') { |v| cw = "#{v}" }
+def PostToot (vis, cw, account, body)
+  uri = URI.parse(account[:host])
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
 
-  opt.parse!(ARGV)
+  req = Net::HTTP::Post.new(uri.request_uri)
+
+  data = {
+            status: body,
+            visibility: vis,
+            spoiler_text: cw
+  }.to_json
+
+
+  token = " Bearer " + account[:token]
+
+  req["Content-Type"] = "application/json"
+  req["Authorization"] = token
+
+  req.body = data
+
+  res = https.request(req)
+
+  $http_status_code = "code -> #{res.code}"
+  $http_msg = "msg -> #{res.message}"
+  $http_body = "body -> #{JSON.parse(res.body)}"
 end
-
-if ARGV[0].nil? || ARGV[0].empty? then
-  puts "Error: ARGV[0] is empty!"
-  exit!
-end 
-
-
-URL = "https://" + host + "/api/v1/statuses"
-
-uri = URI.parse(URL)
-https = Net::HTTP.new(uri.host, uri.port)
-https.use_ssl = true
-
-req = Net::HTTP::Post.new(uri.request_uri)
-
-
-
-data = {
-          status: ARGV[0],
-          visibility: vis,
-          spoiler_text: cw
-       }.to_json
-
-
-token = " Bearer " + token
-
-req["Content-Type"] = "application/json"
-req["Authorization"] = token
-
-req.body = data
-
-res = https.request(req)
-
-puts "code -> #{res.code}"
-puts "msg -> #{res.message}"
-puts "body -> #{JSON.parse(res.body)}"
